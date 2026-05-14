c4eb426bc0637565a89631d73cf75670f04ca102
Frédéric Franchir Madzou <itachimadzou@gmail.com>
2026-05-14 09:48:17 +0200

    feat: docekrisation de l'app Django + PosgreSQL

--- core/settings.py
+++ core/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition
